02afb7fc640427043ca6b4dcfea525db54d7f042
Andreas Pohl <pohl@adtech.de>
2015-02-12 11:12:34 +0100

    dispatcher: make get_worker_by_task public and more doxygen style comments

--- include/tasks/dispatcher.h
+++ include/tasks/dispatcher.h
@@ -41,27 +41,27 @@ class dispatcher {
 
     dispatcher(uint8_t num_workers);
 
-    // Use this method to override the number of worker threads. The default is the
-    // number of CPU's. This method needs to be called before the first call to
-    // instance().
+    /// Use this method to override the number of worker threads. The default is the
+    /// number of CPU's. This method needs to be called before the first call to
+    /// instance().
     static void init_workers(uint8_t num_workers) {
         if (nullptr == m_instance) {
             m_instance = std::make_shared<dispatcher>(num_workers);
         }
     }
 
-    // Set the run mode.
-    // The default is to run a leader/followers system (MODE_SINGLE_LOOP) in
-    // which only one event loop exists that is passed from worker to worker. An
-    // alternative is to run an event loop in each worker (MODE_MULTI_LOOP). This can
-    // improve the responsiveness and throughput in some situations.
-    //
-    // Note: This method has to be called before creating the dispatcher singleton.
-    //       It will fail when called later.
-    //
-    // Available Modes:
-    //   SINGLE_LOOP (Default)
-    //   MULTI_LOOP
+    /// Set the run mode.
+    /// The default is to run a leader/followers system (MODE_SINGLE_LOOP) in
+    /// which only one event loop exists that is passed from worker to worker. An
+    /// alternative is to run an event loop in each worker (MODE_MULTI_LOOP). This can
+    /// improve the responsiveness and throughput in some situations.
+    ///
+    /// Note: This method has to be called before creating the dispatcher singleton.
+    ///       It will fail when called later.
+    ///
+    /// Available Modes:
+    ///   SINGLE_LOOP (Default)
+    ///   MULTI_LOOP
     static void init_run_mode(mode m) {
         if (nullptr != m_instance) {
             terr("ERROR: dispatcher::init_run_mode must be called before anything else!" << std::endl);
@@ -91,44 +91,55 @@ class dispatcher {
         }
     }
 
-    // Add a signal handler.
-    // You have to call this before calling start() or run().
+    /// Add a signal handler.
+    /// You have to call this before calling start() or run().
     static void add_signal_handler(int sig, signal_func_t func);
 
-    // Get a free worker to promote it to the leader.
+    /// Get a free worker to promote it to the leader.
     std::shared_ptr<worker> free_worker();
 
-    // Find a free executor. If non is found a new executor gets created.
+    /// Find a free executor. If non is found a new executor gets created.
     std::shared_ptr<executor> free_executor();
 
-    // When a worker finishes his work he returns to the free worker queue.
+    /// When a worker finishes his work he returns to the free worker queue.
     void add_free_worker(uint8_t id);
 
-    // Returns the last promoted worker from the workers vector. This can be useful
-    // to add tasks in situations where a worker handle is not available.
-    inline worker* last_worker() { return m_workers[m_last_worker_id].get(); }
+    /// Returns the last promoted worker from the workers vector. This can be useful
+    /// to add tasks in situations where a worker handle is not available.
+    inline worker* last_worker() {
+        return m_workers[m_last_worker_id].get();
+    }
 
-    // Add a task to the system.
+    /// Return the worker assigned to a task. If no worker is assigned to the given task, a worker is picked depending
+    /// on the run mode.
+    worker* get_worker_by_task(event_task* task);
+
+    /// Add a task to the system. This method will find out if the task is an event or exec task and handles it
+    /// accordingly.
     void add_task(task* task);
+    /// Add an event task to the system.
     void add_event_task(event_task* task);
+    /// Add an exec task to the system.
     void add_exec_task(exec_task* task);
 
-    // Remove a task from the system.
+    /// Remove a task from the system.
     void remove_task(task* task);
     void remove_event_task(event_task* task);
     void remove_exec_task(exec_task* task);
 
-    // This methods start the system and block until terminate() gets called.
+    /// This method starts the system and blocks until terminate() gets called.
     [[deprecated]] void run(int num, ...);
+
+    /// This method starts the system and blocks until terminate() gets called.
     void run(std::vector<tasks::task*>& tasks);
 
-    // Start the event loop. Do not block.
+    /// Start the event loop. Do not block.
     void start();
 
-    // Wait for the dispatcher to finish
+    /// Wait for the dispatcher to finish.
     void join();
 
-    // Terminate the workers and die.
+    /// Terminate the workers and die.
     inline void terminate() {
         m_term = true;
         m_finish_cond.notify_one();
@@ -140,32 +151,30 @@ class dispatcher {
     static std::shared_ptr<dispatcher> m_instance;
     std::atomic<bool> m_term;
 
-    // All worker threads
+    /// All worker threads
     std::vector<std::shared_ptr<worker> > m_workers;
     uint8_t m_num_workers = 0;
 
-    // All executor threads
+    /// All executor threads
     std::list<std::shared_ptr<executor> > m_executors;
     std::mutex m_executor_mutex;
 
     static mode m_run_mode;
 
-    // State of the workers used for maintaining the leader/followers
+    /// State of the workers used for maintaining the leader/followers
     tools::bitset m_workers_busy;
     tools::bitset::int_type m_last_worker_id = 0;
 
-    // A round robin counter to add tasks to the system. In case each
-    // worker runs it's own event loop this is useful to distribute tasks
-    // across the workers.
+    /// A round robin counter to add tasks to the system. In case each
+    /// worker runs it's own event loop this is useful to distribute tasks
+    /// across the workers.
     std::atomic<uint8_t> m_rr_worker_id;
 
-    // Condition variable/mutex used to wait for finishing up
+    /// Condition variable/mutex used to wait for finishing up
     std::condition_variable m_finish_cond;
     std::mutex m_finish_mutex;
 
     bool m_started = false;
-
-    worker* get_worker_by_task(event_task* task);
 };
 
 }  // tasks
